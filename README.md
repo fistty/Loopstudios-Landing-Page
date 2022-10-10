ccae5bf270b2cdd55efd304a89dcb158c624fcc0
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2022-10-10 18:56:24 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Frontend Mentor - Loopstudios landing page solution
+
+This is a solution to the [Loopstudios landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/loopstudios-landing-page-N88J5Onjw). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+
+### Screenshot
+
+![](./Screenshot.png)
+
+### Links
+
+- Solution URL: [Frontend Solution](https://www.frontendmentor.io/solutions/sunnyside-agency-landing-page-custom-hover-state-and-more-animations-NcI8AbEaD7)
+- Live Site URL: [Netlify Live Site](https://fistty-sunnyside-landing-page.netlify.app/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Mobile-first workflow
+- Flexbox
+- CSS Grid
+- JavaScript
+
+## Author
+
+- Frontend Mentor - [@fistty](https://www.frontendmentor.io/profile/fistty)
---
+++ app.js
@@ -0,0 +1,58 @@
+// const header = document.querySelector("header");
+// console.log([header]);
+// window.addEventListener("resize", () => {
+//   let innerWidth = window.innerWidth;
+//   if (innerWidth > 1440) {
+//     let headerHeight = innerWidth / 2.215;
+//     console.log(typeof headerHeight);
+//     console.log(headerHeight);
+//     header.style.height = `${headerHeight}px`;
+//     console.log(header.clientHeight);
+//     console.log([header]);
+//   } else {
+//     header.style.height = "";
+//   }
+// });
+
+const hamburger = document.querySelector(".hamburger-button");
+const mobileMenu = document.querySelector(".nav-ul");
+const navLink = document.querySelectorAll(".nav-link");
+const body = document.querySelector("body");
+const headerMain = document.querySelector(".header-main");
+const html = document.querySelector("html");
+
+function makeActive() {
+  const ACTIVE = "active";
+  hamburger.classList.toggle(ACTIVE);
+  mobileMenu.classList.toggle(ACTIVE);
+  body.classList.toggle(ACTIVE);
+  headerMain.classList.toggle(ACTIVE);
+  html.classList.toggle(ACTIVE);
+}
+
+function removeActive() {
+  const ACTIVE = "active";
+  hamburger.classList.remove(ACTIVE);
+  mobileMenu.classList.remove(ACTIVE);
+  body.classList.remove(ACTIVE);
+  headerMain.classList.remove(ACTIVE);
+  html.classList.remove(ACTIVE);
+}
+
+hamburger.addEventListener("click", makeActive);
+navLink.forEach((element) => {
+  element.addEventListener("click", removeActive);
+});
+
+// SCROLL IN ANIMATION
+const hiddenElements = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+hiddenElements.forEach((element) => observer.observe(element));
